8947ce1be60e8e5c67fb812d0bdf03634d005418
Ivan Server <evtimovi@lafayette.edu>
2016-09-19 16:31:42 -0400

    fixed bug in basic mnist fully connected one-layer network tutorial

--- basic-mnist/tensorflow-basic-tutorial/network.py
+++ basic-mnist/tensorflow-basic-tutorial/network.py
@@ -9,9 +9,19 @@ if __name__ == "__main__":
     x = tf.placeholder(tf.float32, [None, 784])
     W = tf.Variable(tf.zeros([784,10]))
     b = tf.Variable(tf.zeros([10]))
+    
+    # x is a 1 by 784  matrix of input values
+    # W is a 784 by 10 matrix of weights
+    # b is a 10 by 1 matrix of biases
+    # W*x = 1 by 10 matrix of unbiased outputs
+    # W*x + b is the actual final output (plus biases) as a 10-bit one-hot vector
     y = tf.nn.softmax(tf.matmul(x, W) + b)
+
     y_ = tf.placeholder(tf.float32, [None,10]) #"target" output
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y * tf.log(y), reduction_indices=[1]))
+
+    # note that the y_ is supposed to be the target output, not the actual one (y)
+    # I was missing an underscore here
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
     init = tf.initialize_all_variables()
     sess = tf.Session()
@@ -19,7 +29,8 @@ if __name__ == "__main__":
     for i in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+        if i%100 == 0:
+            correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
     
