f6549d6b8204b5d03eaf08b83239052f320f0c57
Ivan Server <evtimovi@lafayette.edu>
2016-09-24 23:38:55 -0400

    measure accuracy now fully impelemted for LFW dataset

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ This is a repository for the code work to be performed as part of a senior thesi
 The folder basic-mnist contains implementations of the neural network that will be trained to recognize digits from the MNIST data set. These follow the TensorFlow tutorials [here](https://www.tensorflow.org/versions/r0.9/tutorials/mnist/beginners/index.html) and [here](https://www.tensorflow.org/versions/r0.9/tutorials/mnist/pros/index.html#deep-mnist-for-experts).
 
 The folder measure-accuracy contains a script that runs the VGG face networkand then computes its accuracy.
+The intro comment in the script contains details but it can be run with this command:
+
+'''bash
+python measure_accuracy.py lfw/ pairsDevTest.txt
+'''
+
+Also adding the vggface code which is not implemented by me. It came from [here](https://github.com/AKSHAYUBHAT/TensorFace).
--- measure-accuracy/measure_accuracy.py
+++ measure-accuracy/measure_accuracy.py
@@ -1,11 +1,29 @@
 '''
 
-This script is a cleaned-up version of get_feature_vectors.py
+This script is set up to compute to compute the ROC AUC score for a dataset of faces.
+In particular, the following assumptions are made:
+    1. There is a dataset in ../datasets/ that is contained in a folder. 
+    That folder contains a certain number of subfolders each corresponding to 
+    an individual person. Each person's folder contains one or more facial images of the person.
+    This organization follows the organization of the LFW dataset.
+    This is the first argument to the script
+
+    2. There is a test file in ../datasets/ that contains lines of pairs of faces.
+    Each pair contains the exact location of an image relative to the dataset root folder.
+    The locations are separated by an empty space and there are no empty spaces in the path.
+    The location of the file relative to ../datasets/ is the second argument to this script.
+
+    3. An implementation of the VGGFace network is present in ./vggface/
+
+For each input pair, the feature vector of each of the faces is computed and the Euclidean distance between the two is computed.
+This generates a distribution of Euclidean distances which is then used to compute the area under the ROC curve and used as the final score.
 
 '''
 # importat note: numpy and cv2 must be imported before tensorflow because of a bug in tensorflow header loading
 # this bug has been fixed, see here: https://github.com/tensorflow/tensorflow/issues/1924
 import numpy as np
+from scipy.spatial.distance import euclidean 
+from sklearn.metrics import roc_auc_score
 import cv2
 import sys
 import os
@@ -16,42 +34,22 @@ def load_image(path):
     '''
     The following method will load a single image and convert it to grayscale
     '''
-    print '************loading image'
-
     img = cv2.imread('/home/evtimovi/neural-nets-thesis/datasets/lfw/Al_Gore/Al_Gore_0001.jpg')
-
-    print '************loaded image'
-
     img2 = img.astype(np.float32)
-    
-    print '************image cast to float32'
-
     #subtract the average so that all pixels are close to the average
     img2 -= [129.1863,104.7624,93.5940]
-
-
     # have to resize it here to comply with VGGFace specs
     img2 = cv2.resize(img2, (224, 224))
-
-    print '************image resiz done'
-
     img2 = np.array([img2,])  
-
-    print '************after np.array([img2,]'
-
     return img2
 
 
 def get_vector(path, network):
     # load faces
     img1 = load_image(path)
-    
-    print '************loaded image, now starting network eval'
 
     # extract features
     output1 = network.eval(feed_dict={x_image:img1})[0]
-
-    print '************network eval completed'
 
     norm_output1 = output1/np.linalg.norm(output1,2)
     return norm_output1
@@ -72,6 +70,8 @@ def get_paths_from_args():
         sys.exit(1)
 
     DATASETS_BASE = '../datasets/'
+
+    # convert to absolute path for current operating system and location
     path_to_data = os.path.realpath(DATASETS_BASE + sys.argv[1])
     path_to_test = os.path.realpath(DATASETS_BASE + sys.argv[2])
 
@@ -93,13 +93,16 @@ def parse_test_file(path):
             pair = (line_split[0].strip(), line_split[len(line_split)-1].strip())
             image_names.append(pair)
     return image_names
+    
+def setup_network():
+        return network
 
 if __name__ == "__main__":
     
     (path_data, path_test) = get_paths_from_args()
-    names = parse_test_file(path_test)
-  
-    # set up tensorflow network
+    names_with_locations = parse_test_file(path_test)
+    
+    # set up the neural network (cannot be its own method because tensorflow scope)
     # the shape of the placeholder is determined by the VGGFace specs
     # the image has to be resized to fit those
     x_image = tf.placeholder(tf.float32, shape=[1,224,224,3]) 
@@ -110,15 +113,25 @@ if __name__ == "__main__":
     network = vggface.VGGFace()
     network.load(sess, x_image); 
 
-    print '************network loaded'
-
     # restore the variables in initial.ckpt into this sess object
     # this will essentially load the weights and biases
     saver = tf.train.Saver()
-
-    print '************saver initiated'
     saver.restore(sess, "./vggface/initial.ckpt")
-    print '************restore completed'
 
-    print '************calling get vectors'
-    print get_vector(path_data, network)
+    # this array will hold the distribution of distances for each of the 1000 pairs
+    dist_distribution = []
+
+    # go through each pair, get output vector and compute euclidean dist
+    for (lhs, rhs) in names_with_locations:
+        v1 = get_vector(path_data + lhs, network)
+        v2 = get_vector(path_data + rhs, network)
+        dist = euclidean(v1, v2)
+        dist_distribution.append(dist)
+
+    # we will assume by specification that the first 500 pairs are the same person
+    # and the second 500 are different people (i.e. we fed the network the faces of two different people)
+    # that's our ground truth
+    true = [1 for i in xrange(500)]
+    true.extend([0 for i in xrange(500)])
+
+    print 'The ROC AUC score is', roc_auc_score(true, dist_distribution)
