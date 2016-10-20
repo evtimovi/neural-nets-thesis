0368ebce5239ef7ca955acd898b714f00622cf0e
Ivan Server <evtimovi@lafayette.edu>
2016-10-20 11:08:49 -0400

    modified processimgs in util to be more modular

--- util/processimgs.py
+++ util/processimgs.py
@@ -1,17 +1,17 @@
 import numpuy as np
 import cv2
 
-def load_and_crop_image(image_path):
-    '''
-    loads a single image, crops it, normalizes it, resizes it to VGGFace specs,
-    and returns it as a NumPy array 
-    '''
-    face_cascade = cv2.CascadeClassifier('openCVDetector/haarcascade_frontalface_default.xml')
+def load_image_plain(image_path):
     img = cv2.imread(path)
 
     if img is None:
         raise Exception("Image at path " + path + " not found. Check path or numpy, cv2, tensorflow import order.")
-    
+    else:
+        return img
+
+def crop_to_face(img):
+    face_cascade = cv2.CascadeClassifier('openCVDetector/haarcascade_frontalface_default.xml')
+
     gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.equalizeHist(gray, gray)  
     #detect faces. 
@@ -31,13 +31,24 @@ def load_and_crop_image(image_path):
         x2=min(faces[0][tmp][1]+faces[0][tmp][3]+offset,img.shape[0])
         y2=min(faces[0][tmp][0]+faces[0][tmp][2]+offset,img.shape[1])
         img2=img[x1:x2,y1:y2]
-    
+
+    return img2
+
+
+def load_crop_adjust(image_path, width=224, height=224):
+    '''
+    loads a single image, crops it, normalizes it, resizes it to VGGFace specs,
+    and returns it as a NumPy array 
+    '''
+    img = load_image_plain(image_path)
+    img2 = crop_to_face(img)
+        
     img2 = img.astype(np.float32)
     #subtract the average so that all pixels are close to the average
     img2 -= [129.1863,104.7624,93.5940]
 
     # have to resize it here to comply with VGGFace specs
-    img2 = cv2.resize(img2, (224, 224))
+    img2 = cv2.resize(img2, (width, height))
     img2 = np.array([img2,])  
     return img2
 
