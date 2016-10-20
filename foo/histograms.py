b3364738e8c185b92e3c5fe40cd9347569123ac1
Ivan Server <evtimovi@lafayette.edu>
2016-10-20 15:23:07 -0400

    finished measuirng colorferet on plain vgg

--- foo/histograms.py
+++ foo/histograms.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt 
 import numpy as np
-execfile("vgg_vectors.txt")
+execfile('foo/euclidean_dist_colorferet_1000.txt')
 
-flipped_dist=map(lambda x: 1/x, dist_distribution)
-bins = np.arange(0.025,0.070,0.005/4)
+flipped_dist=map(lambda x: 1/x, euclidean_dist)
+bins = np.arange(0.02,0.065,0.005/4)
 plt.hist(flipped_dist[:500], bins=bins, label='genuine')
 plt.hist(flipped_dist[500:], bins=bins, label='imposter')
-plt.xlabel('similarity')
+plt.xlabel('similarity (reciprocal euclidean)')
 plt.ylabel('number of occurences')
 plt.title('Genuine and Imposter Distributions')
 plt.legend()
